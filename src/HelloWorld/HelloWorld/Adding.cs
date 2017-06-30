d52ad4678a87c78ffef8aa619c08b791355a0134
Krzysztof Morcinek <krzysztof.morcinek@gmail.com>
2017-06-30 10:21:49 +0200

    Implement new Adding class

---
+++ src/HelloWorld/HelloWorld/Adding.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    public class Adding
+    {
+        public int Calculate(int x, int y)
+        {
+            if (x >= 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            return x + y;
+        }
+    }
+}
